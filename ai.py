5b854f964fd0f270171a38b3882fcebca41e9fe4
hirokkkawa <hiroaki.kawai@tech.nflabs.jp>
2023-10-02 17:19:29 +0900

    GUIで受け取った質問をChatGPTに投げてCLIで回答を得られるようになった

--- ai.py
+++ ai.py
@@ -7,9 +7,22 @@ import tkinter
 # .envファイルの内容を読み込見込む
 load_dotenv()
 
-# 生成AIに投げたい質問をユーザに入力させる
-# question = input('質問を入力してください: ')
-# print(question)
+# ChatGPTに質問し、回答を表示する関数
+def ChatGPT(question):
+    openai.api_key = os.environ['CHATGPT_API']
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "関西弁で面白く簡潔に質問に短く答えてください"},
+            #{"role": "user", "content": "APIってなに？"},
+            {"role": "user", "content": question},
+        ]   
+    )
+    # ChatGPTからの回答を表示
+    print(f"ChatGPT: {response['choices'][0]['message']['content']}")
+    print(response['usage'])
+
+    return 0
 
 
 # Tkクラス生成
@@ -29,30 +42,20 @@ lbl.place(x=30, y=70)
 txt = tkinter.Entry(width=50)
 txt.place(x=90, y=70)
 
-# ボタンのクリックイベント
+# 送信ボタンの処理関数
 def btn_click():
     print('ボタンがクリックされました')
-    question = str(txt.get())
-    print(question)
+    req_ChatGPT = str(txt.get())
+    print(req_ChatGPT)
+    ChatGPT(req_ChatGPT)
+    return 0
 
 # ボタン作成
-btn = tkinter.Button(root, text='送信', command=btn_click)
+btn = tkinter.Button(root, text='送信', command=lambda:[btn_click()])
 btn.place(x=140, y=170)
 
 # 表示
 root.mainloop()
 print(question)
 
-# # ChatGPTに質問
-# openai.api_key = os.environ['CHATGPT_API']
-# response = openai.ChatCompletion.create(
-#     model="gpt-3.5-turbo",
-#     messages=[
-#         {"role": "system", "content": "関西弁で面白く簡潔に質問に短く答えてください"},
-#         #{"role": "user", "content": "APIってなに？"},
-#         {"role": "user", "content": question},
-#     ]   
-# )
-# # ChatGPTからの回答を表示
-# print(f"ChatGPT: {response['choices'][0]['message']['content']}")
-# print(response['usage'])
+
