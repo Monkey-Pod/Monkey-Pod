08d45bb5f90f21ec275c9dd04e9533cb1d55b543
katsumi.ebina <katsumi.ebina@tech.nflabs.jp>
2023-10-03 18:53:22 +0900

    ChatGPT&PALM2でお互いに解答を確認できるようにした

--- ai.py
+++ ai.py
@@ -1,5 +1,6 @@
-# 必要モジュールのインポート
 import os
+import vertexai
+from vertexai.language_models import TextGenerationModel, CodeChatModel
 from dotenv import load_dotenv
 import openai
 import tkinter
@@ -13,8 +14,7 @@ def ChatGPT(question):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "関西弁で面白く簡潔に質問に短く答えてください"},
-            #{"role": "user", "content": "APIってなに？"},
+            {"role": "system", "content": "このコードの内容を解析して日本語で説明してください"},
             {"role": "user", "content": question},
         ]   
     )
@@ -22,8 +22,58 @@ def ChatGPT(question):
     print(f"ChatGPT: {response['choices'][0]['message']['content']}")
     print(response['usage'])
 
-    return 0
+    return response['choices'][0]['message']['content']
 
+
+def PALM2_to_ChatGPT(question):
+    openai.api_key = os.environ['CHATGPT_API']
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "このコードと解説が正しいかどうかを教えてください"},
+            {"role": "user", "content": question},
+        ]   
+    )
+    # ChatGPTからの回答を表示
+    print(f"PALM2 to ChatGPT: {response['choices'][0]['message']['content']}")
+    print(response['usage'])
+
+    return response['choices'][0]['message']['content']
+
+
+def PALM2(question):
+    vertexai.init(project="monkey-pod-400110", location="us-central1")
+    parameters = {
+        "max_output_tokens": 256,
+        "temperature": 0.2,
+        "top_p": 0.8,
+        "top_k": 40
+    }
+
+    model = CodeChatModel.from_pretrained("codechat-bison@001")
+    code_chat = model.start_chat( context="コードの内容を日本語で説明して", max_output_tokens=500, temperature=0.2, )
+
+    response = code_chat.send_message(question)
+    
+    print(f"PALM2: {response.text}")
+    return response.text
+
+def ChatGPT_to_PALM2(question):
+    vertexai.init(project="monkey-pod-400110", location="us-central1")
+    parameters = {
+        "max_output_tokens": 256,
+        "temperature": 0.2,
+        "top_p": 0.8,
+        "top_k": 40
+    }
+
+    model = CodeChatModel.from_pretrained("codechat-bison@001")
+    code_chat = model.start_chat( context="このコードと解説が正しいかどうかを教えてください", max_output_tokens=500, temperature=0.2, )
+
+    response = code_chat.send_message(question)
+    
+    print(f"ChatGPT to PALM2: {response.text}")
+    return response.text
 
 # Tkクラス生成
 root = tkinter.Tk()
@@ -45,9 +95,14 @@ txt.place(x=90, y=70)
 # 送信ボタンの処理関数
 def btn_click():
     print('ボタンがクリックされました')
-    req_ChatGPT = str(txt.get())
-    print(req_ChatGPT)
-    ChatGPT(req_ChatGPT)
+    req = str(txt.get())
+    print(req)
+    ChatGPT_req = ChatGPT(req)
+    PALM2_req = PALM2(req)
+    # print(ChatGPT_req)
+    # print(PALM2_req)
+    ChatGPT_to_PALM2_req = ChatGPT_to_PALM2(req + ChatGPT_req)
+    PALM2_to_ChatGPT_req = PALM2_to_ChatGPT(req + PALM2_req)
     return 0
 
 # ボタン作成
@@ -57,5 +112,3 @@ btn.place(x=140, y=170)
 # 表示
 root.mainloop()
 print(question)
-
-
